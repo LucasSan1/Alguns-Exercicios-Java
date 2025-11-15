937338c55b316993c06f648ea8af76392f367c1a
LucasSan1 <evangelistalucas034@gmail.com>
2025-11-14 21:25:01 -0300

    Update - Conexão com banco no jogoAdivinha para salvar jogo e puxar ranking

---
+++ JFrame/JogoAdivinha/src/jogoadivinha/TelaTopJogador.java
@@ -0,0 +1,175 @@
+package jogoadivinha;
+
+import java.net.URI;
+import java.net.http.*;
+
+public class TelaTopJogador extends javax.swing.JFrame {
+    
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(TelaTopJogador.class.getName());
+
+    public TelaTopJogador() {
+        initComponents();
+        setLocationRelativeTo(null);
+        setTitle("Recorde");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("O recorde é de:");
+
+        jLabel2.setText("nome");
+
+        jLabel3.setText("Tentativas:");
+
+        jLabel4.setText("Tempo:");
+
+        jLabel5.setText("RA:");
+
+        jLabel6.setText("0");
+
+        jLabel7.setText("0");
+
+        jLabel8.setText("0");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(95, 95, 95)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5))
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel8)
+                    .addComponent(jLabel7)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel2))
+                .addContainerGap(126, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel6))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel7))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel8))
+                .addContainerGap(137, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+    
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+            logger.log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> new TelaTopJogador().setVisible(true));
+    }
+   
+    public void buscarTopJogador() throws Exception {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(new URI(""))
+            .header("apikey", "")
+            .header("Authorization", "Bearer ")
+            .header("Content-Type", "application/json")
+            .GET()
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body().toString();
+        System.out.println("Resposta: " + response.body());
+
+        String nome = extrair(json, "nome");
+        String tentativas = extrair(json, "tentativas");
+        String tempo = extrair(json, "tempo");
+        String ra = extrair(json, "ra");
+
+        jLabel2.setText(nome);
+        jLabel6.setText(tentativas);
+        jLabel7.setText(tempo + " sec");
+        jLabel8.setText(ra);
+
+    }
+   
+    // Gambiarra pra extrair os valores do json :(
+    private static String extrair(String json, String chave) {
+        String busca = "\"" + chave + "\":";
+
+        int start = json.indexOf(busca);
+        if (start == -1) return null;
+
+        start += busca.length();
+
+        char primeiroChar = json.charAt(start);
+
+        if (primeiroChar == '\"') {
+            start++;
+            int end = json.indexOf("\"", start);
+            return json.substring(start, end);
+        }
+
+    
+        int end = start;
+        while (end < json.length() && "0123456789.-".indexOf(json.charAt(end)) != -1) {
+            end++;
+        }
+
+        return json.substring(start, end);
+    }
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    // End of variables declaration//GEN-END:variables
+}
